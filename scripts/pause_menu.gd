3914e78f9943923486f0edfca9db8d6b4e85f66f
JaponBaligi <f.123lee123@gmail.com>
2024-09-22 04:09:12 +0300

    Instead of fire breath and wawes, pause menu added

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,6 +1,5 @@
 extends Control
 #ESCAPE ATTACHED AS "pause_button"
-
 func resume():
 	get_tree().paused = false
 
@@ -15,22 +14,21 @@ func pause_menu():
 		self.visible = false
 		resume()
 
-
 func _on_resume_pressed():
 	self.visible = false
 	resume()
-
-
+	
 func _on_restart_pressed():
 	self.visible = false
 	resume()
 	get_tree().reload_current_scene()
 
-
 func _on_options_pressed():
-	
-	self.visible = false
-
+	$lbl_pause.visible = false
+	# Hide the main pause menu container
+	$PanelContainer.visible = false
+	# Show the settings tab container
+	$Settings_Tab_Container.visible = true
 
 func _on_main_menu_pressed():
 	get_tree().paused = false
@@ -39,3 +37,11 @@ func _on_main_menu_pressed():
 
 func _process(delta):
 	pause_menu()
+
+
+func _on_back_pressed():
+	# Hide the settings tab container
+	$Settings_Tab_Container.visible = false
+	# Show the main pause menu container
+	$PanelContainer.visible = true
+	$lbl_pause.visible = true
